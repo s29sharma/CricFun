7e11c46070023019d62834f2f92fe215347a17c8
sachin <s29sharma@gmail.com>
2018-01-01 23:31:04 -0500

    added a current match activity and fixed home button on mavigation drawer

--- app/src/main/java/com/example/sachinsharma/cricfun/MainActivity.java
+++ app/src/main/java/com/example/sachinsharma/cricfun/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.sachinsharma.cricfun;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -13,6 +15,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -77,12 +81,15 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
+        Fragment fragment = null;
+        Class fragmentClass;
         int id = item.getItemId();
 
         if (id == R.id.nav_home) {
-            // Handle the camera action
+           getApplicationContext();
         } else if (id == R.id.nav_currentMatches) {
-
+           Intent intent=new Intent(this,CurrentActivity.class);
+            startActivity(intent);
         } else if (id == R.id.nav_Results) {
 
         } else if (id == R.id.nav_News) {
@@ -99,8 +106,11 @@ public class MainActivity extends AppCompatActivity
 
         }
 
+
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+
 }
--- app/src/main/java/com/example/sachinsharma/cricfun/MatchesLive.java
+++ app/src/main/java/com/example/sachinsharma/cricfun/MatchesLive.java
@@ -19,7 +19,7 @@ public class MatchesLive {
             matchView1.save();
             MatchView matchView2 = new MatchView("Aus \uD83C\uDDE6\uD83C\uDDFA", "\uD83C\uDDEC\uD83C\uDDE7 Eng", "10:00", "1");
             matchView2.save();
-            MatchView matchView3 = new MatchView("NZ \uD83C\uDDF3\uD83C\uDDFF", "\uD83C\uDDF9\uD83C\uDDF9 WI ", "10:00", "31");
+            MatchView matchView3 = new MatchView("NZ \uD83C\uDDF3\uD83C\uDDFF", "\uD83C\uDDF9\uD83C\uDDF9 WI ", "10:00", "3");
             matchView3.save();
             MatchView matchView4 = new MatchView("Pak \uD83C\uDDF5\uD83C\uDDF0", "\uD83C\uDDEE\uD83C\uDDEA Ire ", "10:00", "2");
             matchView4.save();
